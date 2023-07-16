8155f711d549c0e3d4c3c70d9cf86c2ef10f2a47
mayank010698 <mayankkvpy2014@gmail.com>
2023-07-16 13:00:02 -0500

    updated main

--- DomainNet_ResNet_Exp/cifar_loader.py
+++ DomainNet_ResNet_Exp/cifar_loader.py
@@ -131,8 +131,8 @@ def get_test_loader(data_dir,
     - data_loader: test set iterator.
     """
     normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225],
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
     )
 
     # define transform
